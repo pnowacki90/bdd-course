66c15be5c6c46a6ef08b55f90a92ec90a5442042
Piotr <p.nowacki90@gmail.com>
2018-04-03 13:57:46 +0000

    Added validations to article

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -12,4 +12,18 @@ RSpec.feature "Creating Articles" do
     expect(page).to have_content("Article successfully created!")
     expect(page.current_path).to eq(articles_path)
   end
+  
+  scenario "A user fails to create a ne article" do
+    visit '/'
+
+    click_link "New Article"
+    fill_in "Title of article", with: ""
+    fill_in "Body of article", with: ""
+
+    click_button "Create Article"
+    
+    expect(page).to have_content("Article has not been created!")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
+  end
 end
