d8724dd7a67f88183f12707250692b27b98747a8
Piotr <p.nowacki90@gmail.com>
2018-04-04 11:44:26 +0000

    Added no articles spec in articles index

--- spec/features/listing_articles_spec.rb
+++ spec/features/listing_articles_spec.rb
@@ -17,4 +17,20 @@ RSpec.feature "Listing Articles" do
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
   end
+  
+  scenario "A user has no articles" do
+    Article.delete_all
+    visit articles_path
+    
+    expect(page).not_to have_content(@article1.title)
+    expect(page).not_to have_content(@article2.title)
+    expect(page).not_to have_content(@article1.body)
+    expect(page).not_to have_content(@article2.body)
+    expect(page).not_to have_link(@article1.title)
+    expect(page).not_to have_link(@article2.title)
+    
+    within("h1#no-articles") do
+      expect(page).to have_text("No articles created!")
+    end
+  end
 end
